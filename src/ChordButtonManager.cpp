978a9fb04d043bb93e6667b8f2f6398536afe0ad
satouryouiti <satouryouiti@icloud.com>
2025-07-05 01:40:43 +0900

    VisualChordButton型を作成

--- src/ChordButtonManager.cpp
+++ src/ChordButtonManager.cpp
@@ -2,10 +2,51 @@
 
 using namespace std;
 
+// HACK ラムダ式ではなくヘルパー関数として実装したほうがテストしやすくて無難か
+VirtualChordButton::VirtualChordButton(int button_num)
+    :n
+    (
+        static_cast<Note>
+        (
+            [=]() -> int
+            {
+                enum FifthLocDiff
+                {
+                    Major = 0,
+                    minor = 9,
+                    dim = 5,
+                    arg = 0,
+                    sus = 0
+                };
+                ChordButtonType c = static_cast<ChordButtonType>(button_num / ChordButtonType::end);
+
+                // 五度円上のどこにいるか
+                int loc = button_num % NUM_OF_NOTE;
+                switch (c)
+                {
+                case ChordButtonType::Major:
+                    return loc + FifthLocDiff::Major;
+                    break;
+                case ChordButtonType::minor:
+                    return loc + FifthLocDiff::minor;
+                case ChordButtonType::dim:
+                    return loc + FifthLocDiff::dim;
+                default:
+                    return loc;
+                    break;
+                }
+                return ;
+            }()
+        )
+    )
+    ,cbt(static_cast<ChordButtonType>(button_num / ChordButtonType::end))
+{    
+}
+
 ChordButtonManager::ChordButtonManager()
 {
     real_chord_buttons = vector<Button>(NUM_OF_CHORD_BUTTON);
-    virtual_chord_buttons = set<int>();
+    virtual_chord_buttons = set<VirtualChordButton>();
     no_button_has_been_pressed = true;
 }
 
@@ -24,8 +65,6 @@ bool ChordButtonManager::is_all_button_releaced()
 /// @brief virtual_chord_buttonsをアップデートする
 void ChordButtonManager::update_vcb_state()
 {
-    bool need_to_clear_vcb;
-
     //virtual_chord_buttonsのクリアが必要かどうかを判定し、
     //no_button_has_been_pressedも更新するための場合分け
     if(is_all_button_releaced())
@@ -42,36 +81,15 @@ void ChordButtonManager::update_vcb_state()
             // なにも押されていないところから新たにボタンが押されたので、
             // 全消しして更新を行う
             no_button_has_been_pressed = false;
-            need_to_clear_vcb = true;
-        }
-        else
-        {
-            // ボタンが押されっぱなしの場合
-            need_to_clear_vcb = false;
+            virtual_chord_buttons.clear();
         }
     }
 
-    //HACK
-    if(need_to_clear_vcb)
+    for(int i=0; i < NUM_OF_CHORD_BUTTON; i++)
     {
-        virtual_chord_buttons.clear();
-        for(int i=0; i < NUM_OF_CHORD_BUTTON; i++)
+        if(real_chord_buttons[i].getIsPressed())
         {
-            if(real_chord_buttons[i].getIsPressed())
-            {
-                virtual_chord_buttons.insert(i);
-            }
-        }
-    }
-    else
-    {
-        for(int i; i < NUM_OF_CHORD_BUTTON; i++)
-        {
-            // trueならばそのまま更新しない
-            if(real_chord_buttons[i].getIsPressed())
-            {
-                virtual_chord_buttons.insert(i);
-            }
+            virtual_chord_buttons.insert(VirtualChordButton(i));
         }
     }
 }
--- src/ChordButtonManager.hpp
+++ src/ChordButtonManager.hpp
@@ -7,20 +7,55 @@ const int NUM_OF_CHORD_BUTTON = 60;
 
 const int NUM_OF_NOTE = 12;
 
+enum ChordButtonType
+{
+    Major,
+    minor,
+    dim,
+    arg,
+    sus4,
+    end
+};
+
+enum Note
+{
+    C,
+    G,
+    D,
+    A,
+    E,
+    B,
+    Fs,
+    Df,
+    Af,
+    Ef,
+    Bf,
+    F
+};
+
+struct VirtualChordButton
+{
+    const ChordButtonType cbt;
+    const Note n;
+
+    VirtualChordButton(ChordButtonType type,Note note):cbt(type),n(note){}
+    VirtualChordButton(int button_num);
+};
+
 /// @brief 和音のボタンを配下に持ち、ボタンの読み取り、ボタン押下の継続や記憶などを担当する。
 class ChordButtonManager
 {
     std::vector<Button> real_chord_buttons;
-    std::set<int> virtual_chord_buttons;
+    std::set<VirtualChordButton> virtual_chord_buttons;
     
     bool no_button_has_been_pressed;
-    
-    public:
+
+public:
     ChordButtonManager();
     void update_vcb_state();
     bool is_all_button_releaced();
 
-    std::set<int> getVirtualChordButtons()const{return virtual_chord_buttons;}
+    std::set<VirtualChordButton> getVirtualChordButtons()const{return virtual_chord_buttons;}
 
     /// @brief n番目のボタンの状態（押されているかいないか）をstateにする
     /// @param n ボタンの番号
