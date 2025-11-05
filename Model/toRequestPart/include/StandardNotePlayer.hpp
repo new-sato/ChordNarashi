5d88da91f648da68169171d257acfc4aad860c65
satouryouiti <satouryouiti@icloud.com>
2025-11-05 14:28:10 +0900

    コメントの追加

--- Model/toRequestPart/include/StandardNotePlayer.hpp
+++ Model/toRequestPart/include/StandardNotePlayer.hpp
@@ -7,8 +7,11 @@ class StandardNotePlayer:public InotePlayer
 {
 public:
     StandardNotePlayer() = default;
+    /// @brief オブザーバに、再生するべきものを再生するように要請する
+    /// @param time 0.0のときのみ、再生開始の処理を行う。
     void playNote(float time) override;
+    
+    /// @brief オブザーバーに、現在再生しているノートを止めるように依頼する
     void stopNote() override;
-
     
 };
