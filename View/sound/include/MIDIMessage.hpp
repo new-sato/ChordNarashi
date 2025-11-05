7f8049d11b0ffbcb74d937ffcca538f70e1eafa1
satouryouiti <satouryouiti@icloud.com>
2025-11-05 13:47:31 +0900

    MIDIメッセージ構造体を実装

---
+++ View/sound/include/MIDIMessage.hpp
@@ -0,0 +1,38 @@
+#include "Model.hpp"
+
+using MidiChannelNum = unsigned char;
+
+/// @brief ノートの再生、停止に必要な情報をまとめる
+struct MidiMessage
+{
+    MidiChannelNum midi_channel_num;
+    
+    /// @brief 構造体からメッセージのビット列を作成
+    /// @return 
+    virtual std::vector<unsigned char> message()const = 0;
+};
+
+struct MidiNoteOnMessage: public MidiMessage
+{
+    MidiNoteNum note_number;
+    unsigned char velocity;
+
+    MidiNoteOnMessage(
+        MidiNoteNum note_number_arg,
+        unsigned char velocity_arg=100
+    ):note_number(note_number), velocity(velocity_arg){};
+    
+    std::vector<unsigned char> message() const;
+};
+
+struct MidiNoteOffMessage: public MidiMessage
+{
+    MidiNoteNum note_number;
+    unsigned char velocity;
+
+    MidiNoteOffMessage(
+        MidiNoteNum note_number_arg,
+        unsigned char velocity_arg=100
+    ):note_number(note_number), velocity(velocity_arg){};
+    std::vector<unsigned char> message() const;
+};
---
+++ View/sound/src/MIDIMessage.cpp
@@ -0,0 +1,25 @@
+#include "MIDIMessage.hpp"
+
+std::vector<unsigned char> MidiNoteOnMessage::message()const
+{
+    std::vector<unsigned char> ans;
+    const unsigned char NOTE_ON_MESSAGE = 0x90;
+    
+    ans.push_back(NOTE_ON_MESSAGE + midi_channel_num);
+    ans.push_back(note_number);
+    ans.push_back(velocity);
+
+    return ans;
+}
+
+std::vector<unsigned char> MidiNoteOnMessage::message()const
+{
+    std::vector<unsigned char> ans;
+    const unsigned char NOTE_ON_MESSAGE = 0x80;
+    
+    ans.push_back(NOTE_ON_MESSAGE + midi_channel_num);
+    ans.push_back(note_number);
+    ans.push_back(velocity);
+
+    return ans;
+}
