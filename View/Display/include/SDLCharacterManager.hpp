ed2c6778dcaf6d5d1d8128381b4c39f639d89373
satouryouiti <satouryouiti@icloud.com>
2025-11-09 01:46:06 +0900

    クラスの方針の変更

--- View/Display/include/SDLCharacterManager.hpp
+++ View/Display/include/SDLCharacterManager.hpp
@@ -4,21 +4,15 @@
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
 
+#include <string>
+#include <map>
+
+/// @brief テクスチャの生成とキャッシュを行う。
 class SDLCharacterManager:public IcharacterManager
 {
-    std::vector<SDLTextureData> m_character_to_draw;
-    SDL_Texture* test;
+    std::map<std::string, SDL_Texture*> cash;
+    SDL_Renderer* m_renderer;
 public:
-    SDLCharacterManager()
-    {
-        TTF_Font* font = TTF_OpenFont("Amble-Regular.ttf",20);
-        SDL_Color white={255,255,255,255};
-    }
-    
-    std::vector<SDLTextureData> getCharacterToDraw()const
-    {
-        // TODO テスト
-        SDLTextureData tes(test, 250, 250, 0);
-        return std::vector<SDLTextureData>{tes};
-    };
+    SDLCharacterManager(SDL_Renderer* renderer);
+    SDL_Texture* generateCharaTexture(std::string text, int font_size);
 };
