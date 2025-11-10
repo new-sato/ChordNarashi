bbcabef74916fae60aa4acb106ca62061eb96d9b
satouryouiti <satouryouiti@icloud.com>
2025-11-11 01:00:28 +0900

    変数名とコメントの編集

--- Model/ringButtonTimingManager/src/IringButtonTimingManager.cpp
+++ Model/ringButtonTimingManager/src/IringButtonTimingManager.cpp
@@ -1,6 +1,6 @@
 #include "IringButtonTimingManager.hpp"
 
-void IringButtonTimingManager::setButtonState(bool is_pressed, bool is_sustain_button_pressed_arg)
+void IringButtonTimingManager::setButtonState(bool is_pressed, bool newest_is_sustain_button_pressed)
 {
     //HACK 細かいこと考えるのがめんどくさいのでこうしておく
     is_button_NOW_pressed = false;
@@ -8,8 +8,10 @@ void IringButtonTimingManager::setButtonState(bool is_pressed, bool is_sustain_b
     is_sustain_button_NOW_pressed = false;
     is_sustain_button_NOW_released = false;
     
-    if(is_sustain_button_pressed_arg==true)
+    // 押されているという最新の情報で押されているなら
+    if(newest_is_sustain_button_pressed==true)
     {
+        // さっきまで押されてなかったなら
         if(is_sustain_button_pressed == false)
         {
             is_sustain_button_NOW_pressed = true;
@@ -18,6 +20,8 @@ void IringButtonTimingManager::setButtonState(bool is_pressed, bool is_sustain_b
     }
     else
     {
+        // 以下押されてない場合
+        // さっきまで押されてたなら
         if(is_sustain_button_pressed == true)
         {
             is_sustain_button_NOW_released = true;
