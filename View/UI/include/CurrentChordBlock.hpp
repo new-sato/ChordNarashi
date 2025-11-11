d42866fd2a3b6efac64a49c0b6b935c25dd95c70
satouryouiti <satouryouiti@icloud.com>
2025-11-12 04:40:07 +0900

    現在のキーの表示に成功

--- View/UI/include/CurrentChordBlock.hpp
+++ View/UI/include/CurrentChordBlock.hpp
@@ -9,6 +9,10 @@ class UI;
 class CurrentChordBlock:public Block
 {
     const UI& m_ui;
+
+    std::string m_ringing_chord_name;
+    void setRingingChordName(const ChordName&);
+
     void changeRingingChord(const ChordName&);
 public:
     CurrentChordBlock(const UI&, Model&);
--- View/UI/src/CurrentChordBlock.cpp
+++ View/UI/src/CurrentChordBlock.cpp
@@ -1,9 +1,43 @@
 #include "CurrentChordBlock.hpp"
 #include "UI.hpp"
+#include "map"
+
 
 using namespace std;
 
-void CurrentChordBlock::changeRingingChord(const ChordName &) {}
+std::map<ChordType, string> chord_type_table = {
+    {Major, ""},
+    {minor,"m"},
+    {dim, "dim"},
+    {arg, "arg"},
+    {sus4, "sus4"},
+    {sev, "7"},
+    {Msev,"M7"},
+    {msev,"m7"},
+    {mMsev,"mM7"},
+    {dimsev,"dim7"},
+    {argsev,"arg7"},
+    {sevsus4,"7sus4"},
+    {Msevsus4, "M7sus4"},
+    {add9, "add9"},
+};
+
+void CurrentChordBlock::setRingingChordName(const ChordName &input)
+{
+    string ans;
+    std::string root_note_str = m_ui.get_key_str()[static_cast<int>(input.root_note)];
+    ans.append(root_note_str);
+    
+    std::string chord_type_str = chord_type_table[input.chord_type];
+    ans.append(chord_type_str);
+    
+    m_ringing_chord_name = ans;
+}
+
+void CurrentChordBlock::changeRingingChord(const ChordName &input)
+{
+    setRingingChordName(input);
+}
 
 CurrentChordBlock::CurrentChordBlock(const UI &ui, Model &model)
     : Block(1.0, 1.0, 0, 0.5), m_ui(ui)
@@ -19,5 +53,5 @@ CurrentChordBlock::CurrentChordBlock(const UI &ui, Model &model)
 void CurrentChordBlock::updateBlock(std::chrono::milliseconds)
 {
     m_chara_to_display.clear();
-    m_chara_to_display.push_back(charaDataInBlock(0, 0, 24, "test"));
+    m_chara_to_display.push_back(charaDataInBlock(0, 0, 24, m_ringing_chord_name));
 }
